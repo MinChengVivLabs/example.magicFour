350b8a76a4dc70ac66276b515f33bfc697e2e379
Min Cheng <mcheng@sixfivelabs.com>
2019-03-14 12:26:45 -0700

    Rev 1.02 basic function done

---
+++ code/MagicFour.js
@@ -0,0 +1,41 @@
+// REV 1.01
+
+var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
+    return str.substring(0, str.length-1);
+}
+
+var steps = []
+
+function NextStep(str_number) {
+  if (str_number=='four') {
+    steps.push('Magic FOUR!')
+    return;
+  }
+  else {
+    next_number = inWords(str_number.length)
+    steps.push(str_number + ' becomes ' + next_number + '. ')
+    return NextStep(next_number);
+  }
+}
+
+module.exports.function = function magicFour (str_number) {
+  if (!str_number)
+    return  'Sorry, I need more training.'
+  NextStep(str_number)
+  var rslt_name = ''
+  for (var i=0; i<steps.length; i++) {
+    rslt_name += steps[i]
+  }
+  return rslt_name
+}
